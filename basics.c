111a32a48d675bf204fc0347d6a31745029d78a5
Lavine Oluoch <lavineoluoch@gmail.com>
2023-08-21 18:33:06 -0500

    Create basics.c
    Variables

---
+++ basics.c
@@ -0,0 +1,23 @@
+//Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    printf("Hello World");
+    int age =25;
+    printf("\nAge:%d", age); //format specifier
+    age = 31;
+    printf("\nNew age:%d", age);
+    // Assigning Variables
+    int firstNumber = 33;
+    printf("\nfirstNumber = %d", firstNumber);
+    
+    int secondNumber = firstNumber;
+    printf("\nsecondNumber = %d", secondNumber);
+    
+    // Declare multiple variables at once
+    int var1, var2 = 25;
+    printf("\n%d", var1);
+    printf("\n%d", var2);
+    
+    return 0;
+}
